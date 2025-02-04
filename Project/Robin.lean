65e604cc152032b41ad06fbf2106bc2847720500
Robin Pfeiffer <robin.pfeiffer12@gmail.com>
2025-02-04 17:56:08 +0100

    dont judge this

--- Project/Robin.lean
+++ Project/Robin.lean
@@ -132,75 +132,7 @@ lemma phi_pow_two_iff {n : ℕ} : (∃ m : ℕ, Nat.totient n = 2 ^ m) ↔ (∃ 
 
 #leansearch "(∏ x ∈ A, f x).primeFactors = (f x).primeFactors?"
 
-#check Nat.support_factorization
-lemma phi_pow_two_iff {n : ℕ} : (∃ m : ℕ, Nat.totient n = 2 ^ m) ↔ (∃ s p : ℕ, n = 2^s * p ∧ ProductOfDistinctFermatPrimes p) := by {
-  constructor
-  · intro h
-    obtain ⟨m, hm⟩ := h
-    have hn0 : n ≠ 0 := by {
-      intro h
-      rw [h] at hm
-      simp at hm
-      apply pow_ne_zero m (Ne.symm (Nat.zero_ne_add_one 1))
-      exact id (Eq.symm hm)
-    }
-    by_cases h1 : m = 0
-    . simp [h1] at hm
-      have h2 : n = 1 ∨ n = 2 := by exact Nat.totient_eq_one_iff.mp hm
-      have hfermat : ProductOfDistinctFermatPrimes 1 := by {
-          unfold ProductOfDistinctFermatPrimes
-          simp
-        }
-      obtain h3|h4 := h2
-      · use 0
-        use 1
-        exact ⟨h3, hfermat⟩
-      · use 1
-        use 1
-        exact ⟨h4, hfermat⟩
-    · rw [Nat.totient_eq_div_primeFactors_mul] at hm
-      unfold ProductOfDistinctFermatPrimes
 
-      have hn : (2^m).primeFactors = {2} := by {
-        refine Nat.primeFactors_prime_pow h1 ?hp
-        exact Nat.prime_two
-      }
-      let p1 := ∏ p ∈ n.primeFactors, p
-      let p2 := ∏ p ∈ n.primeFactors, (p - 1)
-      have hhh :  (∏ p ∈ n.primeFactors, p) / ∏ p ∈ n.primeFactors, (p - 1) = ∏ p ∈ n.primeFactors, p / (p - 1) := by {
-        sorry
-
-      }
-      #leansearch "∏ x ∈ A, f x / ∏ x ∈ A, g x = ∏ x ∈ A, f x / g x?"
-      #check Finset.prod_div_distrib
-
-      have hm : n = 2^m * ∏ p ∈ n.primeFactors, p / (p - 1) := by {
-        sorry
-      }
-      use m
-      use ∏ p ∈ n.primeFactors, p / (p - 1)
-      constructor
-      · exact hm
-      · intro p hp
-        sorry
-
-      -- sorry
-
-      -- #check Int.mul_ediv_cancel
-
-
-    -- · rw [h1] at hm
-    --   simp at hm
-    --   obtain ⟨hm1, hm2⟩ := hm
-
-    --
-    -- sorry
-  · intro h
-    obtain ⟨s, p, h1, h2⟩ := h
-    unfold ProductOfDistinctFermatPrimes at h2
-    unfold FermatPrime at h2
-
-}
 
 
 #check 3.factorization.prod
